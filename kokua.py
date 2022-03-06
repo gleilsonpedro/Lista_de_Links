96883b7a6efbf952e64698f1587bafdebb6c73f0
gleilsonpedro <gleilsonsvo@gmail.com>
2022-03-06 12:13:05 -0300

    Criado a idéia de aplicação para abrir links

---
+++ kokua.py
@@ -0,0 +1,18 @@
+import webbrowser
+from tkinter import *
+from tkinter import messagebox
+
+
+
+
+new=2
+url="https://www.scielosp.org/article/sausoc/2017.v26n1/208-217/"
+webbrowser.open(url,new=new)
+
+#####Atualizado#####
+#import webbrowser
+#new=2
+#site =str(input('Digite um site: '))
+#url="https://www."+site+"/"
+#
+#webbrowser.open(url,new=new)
